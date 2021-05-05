a5ef24d32c8d3085061e61f3e82fa70759d81f72
ImedzenneD <imedzenned@yahoo.fr>
2021-05-05 23:21:44 +0100

    three

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask
+from flask import Flask, request
+from datetime import date, datetime
 from buzz import generator
 
 app = Flask(__name__)
@@ -7,8 +8,18 @@ app = Flask(__name__)
 @app.route("/")
 def generate_buzz():
     page = '<html><body style="background-color:powderblue;"><h1>'
+    page += '<br><h1>'
     page += generator.generate_buzz()
-    page += '</h1></body></html>'
+    page += '</h1>'
+    page += '<br>'
+    page += 'User-Agent :'+request.headers.get('User-Agent')
+    page += '<br><h1>'
+    page += '<br>'
+    page += 'User IP Adress :'+request.remote_addr
+    page += '<br><h1>'
+    page += 'Date and Time :'+datetime.now().strftime("%d-%b-%Y (%H:%M:%S.%f)")
+    page += '</h1>'
+    page += '</body></html>'
     return page
 
 if __name__ == "__main__":
